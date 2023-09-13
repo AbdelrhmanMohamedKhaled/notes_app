259ee5761c91484bd1cbbe550d92ef4fd46e1d90
AbdelrhmanMohamedKhaled <abdelrhmankal@gmail.com>
2023-09-13 13:17:49 +0300

    create file adapter for note

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,7 +1,16 @@
-class NoteMdel {
+import 'package:hive/hive.dart';
+
+part 'note_model.g.dart';
+
+@HiveType(typeId: 0)
+class NoteMdel extends HiveObject {
+  @HiveField(0)
   final String title;
+  @HiveField(1)
   final String subTitle;
+  @HiveField(2)
   final String date;
+  @HiveField(3)
   final int color;
 
   NoteMdel(
