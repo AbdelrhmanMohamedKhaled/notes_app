98345f29baef4212ec626056078d7cda6e60c481
AbdelrhmanMohamedKhaled <abdelrhmankal@gmail.com>
2023-09-13 14:41:00 +0300

    add note cubite state

---
+++ lib/cubits/cubit/add_notes_cubit_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'add_notes_cubit_state.dart';
+
+class AddNotesCubitCubit extends Cubit<AddNotesCubitState> {
+  AddNotesCubitCubit() : super(AddNotesCubitInitial());
+}
